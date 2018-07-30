ec4ff125e1d49724ceb39db5c1716260d8854756
Rafael Almeida <rafaelbarbosatec@gmail.com>
2018-07-30 16:47:05 -0300

    adicionado exemplo

--- app/src/main/java/com/fingerprint/MainActivity.kt
+++ app/src/main/java/com/fingerprint/MainActivity.kt
@@ -24,11 +24,21 @@ class MainActivity : AppCompatActivity() {
                     .setIcon(ContextCompat.getDrawable(this,R.mipmap.ic_launcher_round))
                     .setListern(object : EasyFingerPrint.ResultFingerPrintListern{
                         override fun onError(mensage: String, code: Int) {
-                            Toast.makeText(this@MainActivity,"ERROR:$mensage / $code",Toast.LENGTH_SHORT).show()
+
+                            when(code){
+                                EasyFingerPrint.CODE_ERRO_CANCEL -> { } // TO DO
+                                EasyFingerPrint.CODE_ERRO_GREATER_ANDROID_M -> { } // TO DO
+                                EasyFingerPrint.CODE_ERRO_HARDWARE_NOT_SUPPORTED -> { } // TO DO
+                                EasyFingerPrint.CODE_ERRO_NOT_ABLED -> { } // TO DO
+                                EasyFingerPrint.CODE_ERRO_NOT_FINGERS -> { } // TO DO
+                                EasyFingerPrint.CODE_NOT_PERMISSION_BIOMETRIC -> { } // TO DO
+                            }
+
+                            Toast.makeText(this@MainActivity,"Error: $mensage / $code",Toast.LENGTH_SHORT).show()
                         }
 
                         override fun onSucess(cryptoObject: FingerprintManagerCompat.CryptoObject?) {
-                            Toast.makeText(this@MainActivity,"SUCESS",Toast.LENGTH_SHORT).show()
+                            Toast.makeText(this@MainActivity,"Sucess",Toast.LENGTH_SHORT).show()
                         }
 
                     })
